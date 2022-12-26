f3e9ab0bb9072dc1f1876b6bc0c9c45861faa56a
magnificent <me460925@gmail.com>
2022-12-26 07:02:35 -0600

    new iam changes

---
+++ iam-user.tf
@@ -0,0 +1,9 @@
+commitment to coding
+face one at a time massively
+give it your best 
+work and focus at single task daily 
+positive vibes only the act of selling
+mind your own business 
+workout smile and maintain a positive outlook
+go all out on calls and sell your skill
+solve problems you are who you surround yourself with 
